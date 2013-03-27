e262a023e0b6b2ba878b8643eec3a1e1be3f6e7d
Dale Zhao <dale.sleepless.nyc@gmail.com>
2013-03-27 19:10:49 -0400

    Minor refactoring for cloth simulation program.

--- tests/Cloth Simulation 1/cloth_simulation_1.js
+++ tests/Cloth Simulation 1/cloth_simulation_1.js
@@ -1,20 +1,18 @@
-// TODO Adjust parameter to make stepping more stable.
-
+/*
+ * WebGL related variables and constants.
+ * 
+ */
 var gl;
 var program;
-var vertexBuffer;
 var esize = 4 * 3 * 2;
+var vertexBuffer;
 var rawbuf;
 
 /*
- * Mesh.
+ * Mesh geometry and material properties.
  * 
  */
-var positions = [
-		1.0, 0.0, 0.0,
-		0.0, 1.0, 0.0,
-		-1.0, 0.0, 0.0
-];
+var positions = [];
 var idx = [];
 var normals = [];
 var ambient = [0.0, 0.0, 0.0];
@@ -50,19 +48,26 @@ var cameraPosition = [0.0, 1.5, rz];
 var cameraDir = [0.0, -1.0, -rz];
 
 /*
- * Input.
+ * Input related parameters and status.
  * 
  */
 var mousePressed = false;
+var dx = 0.0;
+var dy = 0.0;
 var phi = 0.0;
 var dphi = 0.0;
 var theta = 0.0;
 var dtheta = 0.0;
 var mag = 1 / 300;
-var dx = 0.0;
-var dy = 0.0;
 
-// Cloth intrinsic geometry.
+/*
+ * Cloth.
+ * 
+ */
+
+// Cloth geometry properties.
+
+var pos0 = [-1.0, 1.0, -1.0];
 var sizeu = 2.0;
 var sizev = 2.0;
 var gridu = 0.02;
@@ -76,42 +81,26 @@ var nu = Math.ceil(sizeu / gridu) + 1;
 var nv = Math.ceil(sizev / gridv) + 1;
 var nverts = nu * nv;
 
-// Cloth instance geometry.
-var pos0 = [-1.0, 1.0, -1.0];
-var fixed = [];
-
 // Physics constants.
+
+// The mass of a single vertex.
 var mass = 0.1;
-var g = [0.0, -10, 0.0];
-var resistance = 0.0;
-var stiff = 200.0;
-var dampCo = 0.01;
+var g = [0.0, -1, 0.0];
+var stiff = 100.0;
+var resistanceCo = 0.0;
+var dampCo = 0.0;
 
 // Physics variables.
+
 var xs = [];
+// Positions in the previous step.
 var xs0 = [];
 var vs = [];
 var fs = [];
+var fixed = [];
 
 // Forces.
 
-var wind = function() {
-	
-	for(var i = 0; i < fs.length / 3; ++i) {
-
-		if(fixed[i]) continue;
-		
-		var x0 = xs[i * 3 + 0];
-		var x1 = xs[i * 3 + 1];
-		var x2 = xs[i * 3 + 2];
-
-		// compute wind force.
-
-		fs[i * 3 + 0] += 0.0
-		fs[i * 3 + 1] += 0.0
-		fs[i * 3 + 2] += 0.0
-	}
-};
 var gravity = function() {
 
 	for(var i = 0; i < fs.length / 3; ++i) {
@@ -121,34 +110,15 @@ var gravity = function() {
 		var x0 = xs[3 * i + 0];
 		var x1 = xs[3 * i + 1];
 		var x2 = xs[3 * i + 2];
-		// if(x0 * x0 + x1 * x1 + x2 * x2 < 0.25) continue;
+
+		// We can use a little trick here to make it look as if
+		// the cloth is covering something below it.
 		if(Math.abs(x0) < 0.5 && Math.abs(x2) < 0.25 && x1 < 0.25) continue;
-		
+		else if(x1 < 0) continue;
 		
 		fs[i * 3 + 0] += mass * g[0];
 		fs[i * 3 + 1] += mass * g[1];
 		fs[i * 3 + 2] += mass * g[2];
-	}
-};
-var resistance = function() {
-	
-	for(var i = 0; i < fs.length / 3; ++i) {
-		
-		if(fixed[i]) continue;
-		
-		var x0 = xs[i * 3 + 0];
-		var x1 = xs[i * 3 + 1];
-		var x2 = xs[i * 3 + 2];
-		
-		var v0 = vs[i * 3 + 0];
-		var v1 = vs[i * 3 + 1];
-		var v2 = vs[i * 3 + 2];
-		
-		// compute air resistance.
-		
-		fs[i * 3 + 0] += 0.0
-		fs[i * 3 + 1] += 0.0
-		fs[i * 3 + 2] += 0.0
 	}
 };
 var spring = function() {
@@ -283,6 +253,7 @@ var damp = function() {
 			vs[i * 3 + 2]
 		];
 		
+		// Compute damping force.
 		var f = DGL.v3mult(v, -dampCo);
 		
 		fs[i * 3 + 0] += f[0];
@@ -290,18 +261,56 @@ var damp = function() {
 		fs[i * 3 + 2] += f[2];
 	}
 }
+var wind = function() {
+	
+	for(var i = 0; i < fs.length / 3; ++i) {
+
+		if(fixed[i]) continue;
+		
+		var x0 = xs[i * 3 + 0];
+		var x1 = xs[i * 3 + 1];
+		var x2 = xs[i * 3 + 2];
+
+		// Compute wind force.
+		var f = [0.0, 0.0, 0.0];
+
+		fs[i * 3 + 0] += f[0];
+		fs[i * 3 + 1] += f[1];
+		fs[i * 3 + 2] += f[2];
+	}
+};
+var resistance = function() {
+	
+	for(var i = 0; i < fs.length / 3; ++i) {
+		
+		if(fixed[i]) continue;
+		
+		var v = [
+			vs[i * 3 + 0],
+			vs[i * 3 + 1],
+			vs[i * 3 + 2]
+		];
+		
+		// Compute air resistance.
+		var f = DGL.v3mult(v, -resistanceCo);
+		
+		fs[i * 3 + 0] += f[0];
+		fs[i * 3 + 1] += f[1];
+		fs[i * 3 + 2] += f[2];
+	}
+};
 var forces = [gravity, spring, damp, wind, resistance];
 
 function initCloth() {
 	
-	// Initialize xs and xs0 (equal).
+	// Initialize positions in current and previous steps (equal).
 	xs = positions;
 	xs0 = new Array(3 * nverts);
 	for(var i = 0; i < 3 * nverts; ++i) {
 		xs0[i] = xs[i];
 	}
 	
-	// Initialize vs.
+	// Initialize velocities.
 	vs = new Array(3 * nverts);
 	for(var i = 0; i < nverts; ++i) {
 		vs[3 * i + 0] = 0.0;
@@ -309,13 +318,13 @@ function initCloth() {
 		vs[3 * i + 2] = 0.0;
 	}
 	
-	// Initialize force.
+	// Initialize forces.
 	fs = new Array(3 * nverts);
 	for(var i = 0; i < 3 * nverts; ++i) {
 		fs[i] = 0.0;
 	}
 	
-	// Initialize fixed.
+	// Initialize fixed setup.
 	fixed = new Array(nverts);
 	for(var i = 0; i < nverts; ++i) {
 		fixed[i] = false;
@@ -332,7 +341,6 @@ function initCloth() {
 	fixed[Math.ceil(nu / 2) + (nv - 1) * nu] = true;
 }
 
-// Only physics related code goes into this function.
 function step(dt) {
 	
 	// Clear force.
@@ -340,37 +348,29 @@ function step(dt) {
 		fs[i] = 0.0;
 	}
 	
+	// Apply each force to each vertex.
 	for(var i = 0; i < forces.length; ++i) {		
 		forces[i]();
 	}
 	
-	// Update xs and xs0:
-	// xs0 = xs
+	// Update 'vs'.
+	for(var i = 0; i < 3 * nverts; ++i) {
+		
+		vs[i] += fs[i] / mass * dt;
+	}
+	
+	// Update 'xs0' as 'xs'.
 	for(var i = 0; i < 3 * nverts; ++i) {
 		xs0[i] = xs[i];
 	}
 	
-	// xs = new xs
-	// Verlet Algorithm (explicit integration)
+	// Use Verlet Algorithm (explicit integration) to update positions.
 	// x(t + dt) = 2 * x(t) - x(t - dt) + f / m * dt * dt;
 	for(var i = 0; i < 3 * nverts; ++i) {
 		
 		xs[i] = 2 * xs[i] - xs0[i] + fs[i] / mass * dt * dt;
 	}
-	
-	// Update vs.
-	for(var i = 0; i < 3 * nverts; ++i) {
-		
-		vs[i] += fs[i] / mass * dt;
-	}
 };
-
-function draw() {
-	
-	// fill the buffer with updated data (xs) again
-	// compute the normals and update the buffer
-	// send the buffer to the graphics card	
-}
 
 function setupGl() {
 	
@@ -410,8 +410,7 @@ function setupCamera() {
 		2.4, 1.8, 2.0, 1000));
 	DGL.uniformM4(gl, program, "camera", DGL.camera(
 		cameraPosition, cameraDir, [0.0, 1.0, 0.0]
-	));	
-	
+	));		
 	DGL.uniformV3(gl, program, "cameraPosition", cameraPosition);
 }
 
@@ -429,9 +428,9 @@ function setupMeshes() {
 	normals = mesh.normals;
 	idx = mesh.idx;
 	
-	// Vertices.
 	vertexBuffer = gl.createBuffer();
 	rawbuf = new ArrayBuffer(esize * positions.length / 3);
+
 	var posbuf = DGL.arrayV3(gl, rawbuf, esize, 0, positions);
 	var normbuf = DGL.arrayV3(gl, rawbuf, esize, 12, normals);
 	DGL.bufferArray(gl, vertexBuffer, rawbuf);
@@ -444,7 +443,6 @@ function setupMeshes() {
 	DGL.uniformV3(gl, program, "specular", specular);
 	DGL.uniformF(gl, program, "specPwr", specPwr);
 	
-	// Indices.
 	var indexBuffer = gl.createBuffer();
 	DGL.bufferIdxArray(gl, indexBuffer, new Uint16Array(idx));
 }
@@ -459,33 +457,24 @@ function setupInput() {
 		if(mousePressed) dphi = dx * mag;
 		
 	}, program, {
+		
 		viewportX: "viewportX",
 		viewportY: "viewportY",
 		scaledViewportX: "scaledViewportX",
 		scaledViewportY: "scaledViewportY"
 	});
 	
-	DGL.addMouseDown(gl, function(e) {
-		
-		mousePressed = true;
-		
-	}, program, {
-		mousePressed: "mousePressed"
-	});
+	DGL.addMouseDown(gl, function(e) { mousePressed = true; },
+		program, { mousePressed: "mousePressed" });
 	
-	DGL.addMouseUp(gl, function(e) {
-		
-		mousePressed = false;
-		
-	}, program, {
-		mousePressed: "mousePressed"
-	});
+	DGL.addMouseUp(gl, function(e) { mousePressed = false; },
+		program, { mousePressed: "mousePressed" });
 }
 
 function updateCamera() {
 	
 	phi += dphi;
-
+	
 	if(phi < 0) phi += Math.PI * 2;
 	if(phi > Math.PI * 2) phi -= Math.PI * 2;
 	
@@ -505,66 +494,130 @@ function updateCamera() {
 
 function updateNormals() {
 	
-	// TODO Correct normal computation (by using averaging).
 	for(var v = 0; v < nv - 1; ++v) {
 		for(var u = 0; u < nu - 1; ++u) {
+
+			var i = nu * v + u;
+			var i1 = nu * v + u + 1;
+			var i2 = nu * (v + 1) + u + 1;
+			var i3 = nu * (v + 1) + u;
+			var i4 = nu * (v + 1) + u - 1;
+			var i5 = nu * v + u - 1;
+			var i6 = nu * (v - 1) + u - 1;
+			var i7 = nu * (v - 1) + u;
+			var i8 = nu * (v - 1) + u + 1;
 			
-			var i1 = nu * v + u;
-			var i2 = nu * (v + 1) + u;
-			var i3 = nu * (v + 1) + u + 1;
-			var i4 = nu * v + u + 1;
-			
-			var pos1 = [
-				xs[i1 * 3 + 0],
-				xs[i1 * 3 + 1],
-				xs[i1 * 3 + 2]
+			var pos = [
+				xs[3 * i + 0],
+				xs[3 * i + 1],
+				xs[3 * i + 2]
 			];
+			
+			var nn = 0;
+			var normal = [0.0, 0.0, 0.0];
+			
+			if(u + 1 < nu && v + 1 < nv) {
 				
-			var pos2 = [
-				xs[i2 * 3 + 0],
-				xs[i2 * 3 + 1],
-				xs[i2 * 3 + 2]
-			];
-			
-			var pos3 = [
-				xs[i3 * 3 + 0],
-				xs[i3 * 3 + 1],
-				xs[i3 * 3 + 2]
-			];
-			
-			var pos4 = [
-				xs[i4 * 3 + 0],
-				xs[i4 * 3 + 1],
-				xs[i4 * 3 + 2]
-			];
-			
-			var n1 = DGL.triangleNormal(pos1, pos2, pos3);
-			n1 = DGL.v3add(n1, DGL.triangleNormal(pos1, pos3, pos4));
-			n1 = DGL.v3add(n1, DGL.triangleNormal(pos1, pos2, pos3));
-			n1 = DGL.v3add(n1, DGL.triangleNormal(pos2, pos3, pos4));
-			n1 = DGL.v3mult(n1, 0.25);
-			var n4 = n1;
-			
-			normals[3 * i1 + 0] = n1[0];
-			normals[3 * i1 + 1] = n1[1];
-			normals[3 * i1 + 2] = n1[2];
-			normals[3 * i4 + 0] = n4[0];
-			normals[3 * i4 + 1] = n4[1];
-			normals[3 * i4 + 2] = n4[2];
+				var pos1 = [
+					xs[3 * i1 + 0],
+					xs[3 * i1 + 1],
+					xs[3 * i1 + 2]
+				];				
+				var pos2 = [
+					xs[3 * i2 + 0],
+					xs[3 * i2 + 1],
+					xs[3 * i2 + 2]
+				];
+				var pos3 = [
+					xs[3 * i3 + 0],
+					xs[3 * i3 + 1],
+					xs[3 * i3 + 2]
+				];
+				
+				normal = DGL.v3add(normal, DGL.triangleNormal(pos, pos1, pos2));
+				normal = DGL.v3add(normal, DGL.triangleNormal(pos, pos2, pos3));
+			}
+			if(v + 1 < nv && u - 1 >= 0) {
+				
+				var pos1 = [
+					xs[3 * i3 + 0],
+					xs[3 * i3 + 1],
+					xs[3 * i3 + 2]
+				];				
+				var pos2 = [
+					xs[3 * i4 + 0],
+					xs[3 * i4 + 1],
+					xs[3 * i4 + 2]
+				];
+				var pos3 = [
+					xs[3 * i5 + 0],
+					xs[3 * i5 + 1],
+					xs[3 * i5 + 2]
+				];
+				
+				normal = DGL.v3add(normal, DGL.triangleNormal(pos, pos1, pos3));
+			}
+			if(u - 1 >= 0 && v - 1 >= 0) {
+				
+				var pos1 = [
+					xs[3 * i5 + 0],
+					xs[3 * i5 + 1],
+					xs[3 * i5 + 2]
+				];				
+				var pos2 = [
+					xs[3 * i6 + 0],
+					xs[3 * i6 + 1],
+					xs[3 * i6 + 2]
+				];
+				var pos3 = [
+					xs[3 * i7 + 0],
+					xs[3 * i7 + 1],
+					xs[3 * i7 + 2]
+				];
+				
+				normal = DGL.v3add(normal, DGL.triangleNormal(pos, pos1, pos2));
+				normal = DGL.v3add(normal, DGL.triangleNormal(pos, pos2, pos3));
+			}
+			if(v - 1 >= 0 && u + 1 < nu) {
+				
+				var pos1 = [
+					xs[3 * i7 + 0],
+					xs[3 * i7 + 1],
+					xs[3 * i7 + 2]
+				];				
+				var pos2 = [
+					xs[3 * i8 + 0],
+					xs[3 * i8 + 1],
+					xs[3 * i8 + 2]
+				];
+				var pos3 = [
+					xs[3 * i1 + 0],
+					xs[3 * i1 + 1],
+					xs[3 * i1 + 2]
+				];
+				
+				normal = DGL.v3add(normal, DGL.triangleNormal(pos, pos1, pos3));
+			}
+			normal = DGL.v3normalize(normal);
+			normals[3 * i + 0] = -normal[0];
+			normals[3 * i + 1] = -normal[1];
+			normals[3 * i + 2] = -normal[2];
 		}
 	}
 }
 
 function update() {
 	
-	// TODO
-	// Step the physics!
+	// Step the physics.
 	step(0.01);
+	
 	// Update normals.
 	updateNormals();
 	
+	// Update the camera.
 	updateCamera();
 	
+	// Copy the updated position and normal arrays into the buffer.
 	var posbuf = DGL.arrayV3(gl, rawbuf, esize, 0, positions);
 	var normbuf = DGL.arrayV3(gl, rawbuf, esize, 12, normals);
 	DGL.bufferArray(gl, vertexBuffer, rawbuf);
@@ -572,12 +625,14 @@ function update() {
 
 function onload() {
 	
+	// WebGL setup.
 	setupGl();
 	setupLights();
 	setupCamera();
 	setupMeshes();
 	setupInput();
 	
+	// Cloth simulation setup.
 	initCloth();
 	
 	DGL.loop(function() {
